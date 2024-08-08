d668689613f2c0ee0f9559cab2f02bb4bec32a51
Axel Martin <axel.martin@outlook.com>
2024-08-08 10:36:31 +0200

    Update next.config.mjs with next-intl plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/*
+Ce fichier configure Next.js en ajoutant des fonctionnalités spécifiques via des plugins, comme 'next-intl' qui permet de gérer les langues et les traductions dans l'application.
+Il sert de point central pour intégrer le plugin a la configuration de Next.
+*/
 
-export default nextConfig;
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Autres configurations Next.js ici
+};
+
+export default withNextIntl(nextConfig);
