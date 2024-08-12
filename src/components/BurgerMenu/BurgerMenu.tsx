c27547764e1dbf806a59feee8db3d51c5e47ae40
Axel Martin <axel.martin@outlook.com>
2024-08-12 15:53:40 +0200

    add comments to BurgerMenu.tsx and BurgerMenu.module.scss

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,46 +1,15 @@
-// 'use client';
-
-// import React, { useState } from 'react';
-// import styles from './BurgerMenu.module.scss';
-
-// export default function BurgerMenu() {
-//   // Etat local pour gérer l'ouverture/fermeture du burgerMenu
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   // Fonction pour inverser l'état du burgerMenu (ouvrir/fermer)
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <>
-//       <button
-//         className={`${styles.burger} ${isOpen ? styles.open : ''}`}
-//         onClick={toggleMenu}
-//       >
-//         <div className={`${styles['burger-line']} ${styles.line1}`}></div>
-//         <div className={`${styles['burger-line']} ${styles.line2}`}></div>
-//         <div className={`${styles['burger-line']} ${styles.line3}`}></div>
-//       </button>
-//       <div className={`${styles.menuPanel} ${isOpen ? styles.open : ''}`}>
-//         <div className={styles.menuContent}>
-//           <p>Bonjour panneau</p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 'use client';
 
 import React, { useState } from 'react';
 import styles from './BurgerMenu.module.scss';
 
 export default function BurgerMenu() {
+  // Déclare un etat pour gérer l'ouverture/fermeture du menu burger. (false est la valeur initiale et isActive détermine si le buger est ouvert ou fermé)
   const [isActive, setIsActive] = useState(false);
 
+  // Fonction qui inverse l'état 'isActive' -> Si le menu est fermé (isActive = false) il s'ouvre et vice-versa
   const toggleActive = () => {
-    setIsActive(!isActive);
+    setIsActive(!isActive); // Met à jour l'état avec la valeur opposée de `isActive`
   };
 
   return (
@@ -48,6 +17,10 @@ export default function BurgerMenu() {
       <button
         className={`${styles.buttonContainer} ${isActive ? styles.active : ''}`}
         onClick={toggleActive}
+        aria-expanded={isActive} // Indique l'état du menu (ouvert/fermé)
+        aria-controls="burger-menu" // Lie le bouton au panneau du menu
+        aria-label="Toggle menu" // Description de l'action du bouton
+        role="button" // Rôle explicitement défini
       >
         <div className={`${styles.burger} ${isActive ? styles.open : ''}`}>
           <div className={`${styles['burger-line']} ${styles.line1}`}></div>
