8421f1d5cbdd9bcf4d3b9d773b30916c7bef7073
Axel Martin <axel.martin@outlook.com>
2024-08-16 15:23:09 +0200

    Create half-circle design logo into Welcome component

--- src/components/Sections/Welcome/Welcome.tsx
+++ src/components/Sections/Welcome/Welcome.tsx
@@ -1,42 +1,13 @@
-// import React from 'react';
-// import styles from './Welcome.module.scss';
-
-// export default function Welcome() {
-//   return (
-//     <div className={styles.welcomeContainer}>
-//       <div className={styles.presentationContainer}>
-//         <div className={styles.circleContainer}>
-//           <div className={styles.halfCircle}></div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// import React from 'react';
-// import styles from './Welcome.module.scss';
-
-// export default function Welcome() {
-//   return (
-//     <div className={styles.welcomeContainer}>
-//       <div className={styles.circlesContainer}>
-//         <div className={styles.circle}></div>
-//         <div className={styles.lightCircle}></div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from 'react';
 import styles from './Welcome.module.scss';
 
 export default function Welcome() {
   return (
-    <div className={styles.welcomeContainer}>
-      <div className={styles.circlesContainer}>
+    <section className={styles.welcomeSection}>
+      <div className={styles.contentContainer}>
         <div className={styles.circle}></div>
-        <div className={styles.shadowsCircle}></div>
+        <div className={styles.shadowCircle}></div>
       </div>
-    </div>
+    </section>
   );
 }
