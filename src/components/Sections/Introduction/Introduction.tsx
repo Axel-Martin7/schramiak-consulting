02fca80da451281c1d85504928a6cda4fec068c1
Axel Martin <axel.martin@outlook.com>
2024-08-20 13:28:52 +0200

    Create polygons for 3d shadows effect on diamonds

--- src/components/Sections/Introduction/Introduction.tsx
+++ src/components/Sections/Introduction/Introduction.tsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import styles from './WelcomeSquares.module.scss';
-
-// export default function WelcomeSquares() {
-//   return (
-//     <section className={styles.welcomeSection}>
-//       <div className={styles.squaresContainer}>
-//         <div className={styles.contentSquare}></div>
-//         <div className={styles.square}>
-//           <div className={styles.textContainer}>coucou toi</div>
-//           <div className={styles.square2}></div>
-//           <div className={styles.square3}></div>
-//           <div className={styles.square4}></div>
-//           <div className={styles.square5}></div>
-//           <div className={styles.square6}></div>
-//           <div className={styles.square7}></div>
-
-//           <div className={styles.triange6}></div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 import React from 'react';
 import styles from './Introduction.module.scss';
 
@@ -39,6 +15,12 @@ export default function Introduction() {
           <div className={styles.diamond5}></div>
           <div className={styles.diamond6}></div>
           <div className={styles.diamond7}></div>
+
+          <div className={styles.polygon3}></div>
+          <div className={styles.polygon4}></div>
+          <div className={styles.polygon5}></div>
+          <div className={styles.polygon6}></div>
+          <div className={styles.polygon7}></div>
         </div>
       </div>
     </section>
