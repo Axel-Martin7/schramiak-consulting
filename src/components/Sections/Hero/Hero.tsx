e7832fd479a11b0a843aa415e1fb0643d0ac15e7
Axel Martin <axel.martin@outlook.com>
2024-08-22 16:08:15 +0200

    Create overlayContainer polygon

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -1,3 +1,33 @@
+// import React from 'react';
+// import styles from './Hero.module.scss';
+// import Image from 'next/image';
+// import ampoule from '../../../../public/ampoule.jpg';
+// import { barlow } from '@/styles/font';
+
+// export default function Hero() {
+//   return (
+//     <section className={styles.heroSection}>
+//       <div className={styles.mainContainer}>
+//         <div className={styles.contentContainer}>Content</div>
+//         <div className={styles.imageContainer}>
+//           <Image
+//             className={styles.image}
+//             alt="image ampoule"
+//             src={ampoule}
+//             placeholder="blur"
+//             quality={100}
+//             fill
+//             sizes="100vh"
+//           ></Image>
+//         </div>
+//       </div>
+
+//       <div className={styles.shadowElipseTop}></div>
+//       <div className={styles.shadowElipseBottom}></div>
+//     </section>
+//   );
+// }
+
 import React from 'react';
 import styles from './Hero.module.scss';
 import Image from 'next/image';
@@ -8,17 +38,22 @@ export default function Hero() {
   return (
     <section className={styles.heroSection}>
       <div className={styles.mainContainer}>
-        <div className={styles.contentContainer}>Content</div>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            alt="image ampoule"
-            src={ampoule}
-            placeholder="blur"
-            quality={100}
-            fill
-            sizes="100vh"
-          ></Image>
+        <div className={styles.overlayContainer}>
+          <div className={styles.contentContainer}></div>
+        </div>
+
+        <div className={styles.imageContainerPositioner}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.image}
+              alt="image ampoule"
+              src={ampoule}
+              placeholder="blur"
+              quality={100}
+              fill
+              sizes="100vh"
+            ></Image>
+          </div>
         </div>
       </div>
 
