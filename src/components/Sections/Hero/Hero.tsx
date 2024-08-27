66767feda76b00d2c8a8a4c240f989e91a2cd880
Axel Martin <axel.martin@outlook.com>
2024-08-27 13:35:52 +0200

    Create responsive polygon Container

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -53,6 +53,19 @@ export default function Hero() {
     <section className={styles.heroSection}>
       <div className={styles.mainContainer}>
         <div className={styles.overlayContainer}></div>
+        <div className={styles.contentContainer}>
+          <h1> Bonjour voici un titre qui donne envie de cliquer</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi,
+            quos repellat impedit optio error harum corrupti praesentium
+            quibusdam voluptate nostrum repudiandae, id, libero quae
+            reprehenderit eligendi vel excepturi. Qui, ad? Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Quis perferendis dolore, labore
+            recusandae vitae distinctio dolor odio quae iste voluptatibus soluta
+            at, qui aperiam illum totam necessitatibus tempora laudantium
+            obcaecati!
+          </p>
+        </div>
       </div>
     </section>
   );
