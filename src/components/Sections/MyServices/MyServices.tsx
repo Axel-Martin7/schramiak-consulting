203b8f6e6364f7f69d1455e50b40273d1189d8ba
Axel Martin <axel.martin@outlook.com>
2024-09-02 17:46:06 +0200

    Commit fin de journée

--- src/components/Sections/MyServices/MyServices.tsx
+++ src/components/Sections/MyServices/MyServices.tsx
@@ -1,3 +1,32 @@
+// import React from 'react';
+// import styles from './MyServices.module.scss';
+
+// export default function Myservices() {
+//   return (
+//     <section className={styles.servicesSection}>
+//       <div className={styles.shapesContainer}>
+//         <div className={styles.arrow}></div>
+//         <div className={styles.hexagonBack1}>
+//           <div className={styles.hexagon}></div>
+//         </div>
+//         <div className={styles.hexagonBack2}>
+//           <div className={styles.hexagon}></div>
+//         </div>
+
+//         <div className={styles.hexagonBack3}>
+//           <div className={styles.hexagon}></div>
+//         </div>
+//         <div className={styles.hexagonBack4}>
+//           <div className={styles.hexagon}></div>
+//         </div>
+//         <div className={styles.centralHexagon}></div>
+//         <div className={styles.helpHexagon}></div>
+//         <div className={styles.helpHexagon2}></div>
+//       </div>
+//     </section>
+//   );
+// }
+
 import React from 'react';
 import styles from './MyServices.module.scss';
 
@@ -5,15 +34,25 @@ export default function Myservices() {
   return (
     <section className={styles.servicesSection}>
       <div className={styles.shapesContainer}>
+        {/* Conteneur en forme de fléche */}
         <div className={styles.arrow}></div>
-        <div className={styles.hexagon1}></div>
-        <div className={styles.hexagon2}></div>
 
-        <div className={styles.hexagon3}></div>
-        <div className={styles.hexagon4}></div>
+        {/* Hexagones principaux */}
+        <div className={styles.borderHexagon1}></div>
+        <div className={styles.borderHexagon2}></div>
+        <div className={styles.borderHexagon3}></div>
+        <div className={styles.borderHexagon4}></div>
+
+        {/* Hexagones des ombrages */}
+        <div className={styles.shadowHexagon1}></div>
+
+        <div className={styles.shadowHexagon3}></div>
+        <div className={styles.shadowHexagon4}></div>
+
+        {/* Hexagones de repères */}
         <div className={styles.centralHexagon}></div>
-        <div className={styles.helpHexagon}></div>
-        <div className={styles.helpHexagon2}></div>
+        <div className={styles.helperHexagon1}></div>
+        <div className={styles.helperHexagon2}></div>
       </div>
     </section>
   );
