faa3867b5cdb896816f466b03a7413c528e76ce4
Axel Martin <axel.martin@outlook.com>
2024-09-03 16:43:55 +0200

    Added hexagones shapes on borderHexagons with background gradient

--- src/components/Sections/MyServices/MyServices.tsx
+++ src/components/Sections/MyServices/MyServices.tsx
@@ -3,39 +3,24 @@ import styles from './MyServices.module.scss';
 
 export default function Myservices() {
   return (
-    // <section className={styles.servicesSection}>
-    //   <div className={styles.shapesContainer}>
-    //     {/* Conteneur en forme de fléche */}
-    //     <div className={styles.arrow}></div>
-
-    //     {/* Hexagones principaux */}
-    //     <div className={styles.borderHexagon1}></div>
-    //     <div className={styles.borderHexagon2}></div>
-    //     <div className={styles.borderHexagon3}></div>
-    //     <div className={styles.borderHexagon4}></div>
-
-    //     {/* Formes des ombrages */}
-    //     <div className={styles.shadowShape1}></div>
-    //     <div className={styles.shadowShape2}></div>
-    //     <div className={styles.shadowShape3}></div>
-    //     <div className={styles.shadowShape4}></div>
-
-    //     {/* Hexagones de repères */}
-    //     <div className={styles.centralHexagon}></div>
-    //     <div className={styles.helperHexagon1}></div>
-    //     <div className={styles.helperHexagon2}></div>
-    //   </div>
-    // </section>
     <section className={styles.servicesSection}>
       <div className={styles.shapesContainer}>
         {/* Conteneur en forme de fléche */}
         <div className={styles.arrow}></div>
 
         {/* Hexagones principaux */}
-        <div className={styles.borderHexagon1}></div>
-        <div className={styles.borderHexagon2}></div>
-        <div className={styles.borderHexagon3}></div>
-        <div className={styles.borderHexagon4}></div>
+        <div className={styles.borderHexagon1}>
+          <div className={styles.hexagon}></div>
+        </div>
+        <div className={styles.borderHexagon2}>
+          <div className={styles.hexagon}></div>
+        </div>
+        <div className={styles.borderHexagon3}>
+          <div className={styles.hexagon}></div>
+        </div>
+        <div className={styles.borderHexagon4}>
+          <div className={styles.hexagon}></div>
+        </div>
 
         {/* Hexagones de repères */}
         <div className={styles.centralHexagon}></div>
