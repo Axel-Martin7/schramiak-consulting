2c8954c9f8f0f13cc3efd1669137c39d83ab0e07
Axel Martin <axel.martin@outlook.com>
2024-09-04 13:40:21 +0200

    added services informations inside hexagon shapes

--- src/components/Sections/MyServices/MyServices.tsx
+++ src/components/Sections/MyServices/MyServices.tsx
@@ -15,17 +15,16 @@ export default function Myservices() {
           <div className={styles.arrow}>
             <div className={styles.contentContainer}>
               <h1 className={`${styles.title} ${barlow.className}`}>
-                Mes Services
+                <span className={styles.colored}>Mes</span> services
               </h1>
-              <p className={styles.description}>
-                En tant que consultant, mon role sera de mettre en place les
-                meilleures stratégies et outils nécessaire à l'atteinte de vos
-                objectifs ainsi que ceux de vos clients dans un climat de
-                confidentialité, transparence et collaboration pour optimiser
-                vos ressources et soutenir vos équipes
+              <p className={`${styles.description} ${barlow.className}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus autem consequuntur minus libero! Debitis error
+                dignissimos dolor sint, tempore eaque beatae molestias esse quam
+                provident veritatis aliquid natus harum laboriosam.
               </p>
-              <div className={styles.btnContainer}>
-                <Button />
+              <div className={styles.buttonContainer}>
+                <Button label="Learn more" variant="primary" />
               </div>
             </div>
           </div>
@@ -35,57 +34,67 @@ export default function Myservices() {
         <div className={styles.borderHexagon1}>
           <div className={styles.hexagon}>
             <div className={styles.contentContainer}>
-              <p className={styles.number}>01</p>
-              <h2 className={`${styles.serviceTitle} ${barlow.className}`}>
-                CFO / Controleur financier
-              </h2>
-              <p className={styles.description}>
+              <p className={`${styles.number} ${barlow.className}`}>01</p>
+
+              <h1 className={`${styles.serviceTitle} ${barlow.className}`}>
+                <span className={styles.colored}>CFO</span> / Controleur
+                financier
+              </h1>
+              <p className={`${styles.description} ${barlow.className}`}>
                 Renforcement de votre équipe par une présence à temps partiel
                 dans vos locaux
               </p>
             </div>
           </div>
         </div>
+
         <div className={styles.borderHexagon2}>
           <div className={styles.hexagon}>
             <div className={styles.contentContainer}>
-              <p className={styles.number}>02</p>
-              <h2 className={`${styles.serviceTitle} ${barlow.className}`}>
-                Rentabilité & gestion des risques
-              </h2>
-              <p className={styles.description}>
+              <p className={`${styles.number} ${barlow.className}`}>02</p>
+
+              <h1 className={`${styles.serviceTitle} ${barlow.className}`}>
+                <span className={styles.colored}>Rentabilité</span> & gestion
+                des risques
+              </h1>
+              <p className={`${styles.description} ${barlow.className}`}>
                 Analyse de la chaine de valeur et de la rentabilité. <br></br>
-                Gestion des risques & amélioration de l'efficacité
+                Gestion des risques & amélioration de l efficacité
                 opérationnelle.
               </p>
             </div>
           </div>
         </div>
+
         <div className={styles.borderHexagon3}>
           <div className={styles.hexagon}>
             <div className={styles.contentContainer}>
-              <p className={styles.number}>03</p>
-              <h2 className={`${styles.serviceTitle} ${barlow.className}`}>
-                Gestion de projet
-              </h2>
-              <p className={styles.description}>
-                Création, implémentation et déploiement de solutions et
+              <p className={`${styles.number} ${barlow.className}`}>03</p>
+
+              <h1 className={`${styles.serviceTitle} ${barlow.className}`}>
+                <span className={styles.colored}>Gestion</span> de projet
+              </h1>
+              <p className={`${styles.description} ${barlow.className}`}>
+                Création, implémentation et déploiement de solutions et de
                 processus sur mesure. <br></br>
                 Développement logiciels métiers.
               </p>
             </div>
           </div>
         </div>
+
         <div className={styles.borderHexagon4}>
           <div className={styles.hexagon}>
             <div className={styles.contentContainer}>
-              <p className={styles.number}>04</p>
-              <h2 className={`${styles.serviceTitle} ${barlow.className}`}>
-                Planification financière & Buisiness plan
-              </h2>
-              <p className={styles.description}>
-                Vision strégique et financière 3/5ans. <br></br>
-                Accompagnement financements
+              <p className={`${styles.number} ${barlow.className}`}>04</p>
+
+              <h1 className={`${styles.serviceTitle} ${barlow.className}`}>
+                <span className={styles.colored}>Planification financière</span>{' '}
+                & business plan
+              </h1>
+              <p className={`${styles.description} ${barlow.className}`}>
+                Vision stratégique et financière 3/5 ans. <br></br>
+                Accompagnement financements.
               </p>
             </div>
           </div>
@@ -110,20 +119,6 @@ export default function Myservices() {
           ></Image>
         </div>
       </div>
-
-      {/* <div className={styles.backgroundContainer}>
-        <Image
-          className={styles.image}
-          alt="image ampoule"
-          src={map}
-          placeholder="blur"
-          quality={100}
-          fill
-          sizes="100vw"
-        ></Image>
-
-        <div className={styles.backgroundGradient}></div>
-      </div> */}
     </section>
   );
 }
