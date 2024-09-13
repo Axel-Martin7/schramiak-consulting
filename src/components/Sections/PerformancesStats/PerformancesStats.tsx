89d4b5a8e1f77331479e5f0f5be9ba1ed0a995a4
Axel Martin <axel.martin@outlook.com>
2024-09-13 16:08:41 +0200

    created structure for circles objects on PerformancesStats component

--- src/components/Sections/PerformancesStats/PerformancesStats.tsx
+++ src/components/Sections/PerformancesStats/PerformancesStats.tsx
@@ -3,27 +3,13 @@ import styles from './PerformancesStats.module.scss';
 
 export default function PerformancesStats() {
   return (
-    // <section className={styles.statsSection}>
-    //   <div className={styles.mainContainer}>
-    //     <div className={styles.objectsContainer}>
-    //       <div className={styles.object1Container}>
-    //         <div className={styles.demiCircle}>
-    //           <div className={styles.circle}></div>
-    //           <div className={styles.demiCircleReflection}></div>
-    //           <div className={styles.shadow}></div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
-
     <section className={styles.statsSection}>
       <div className={styles.mainContainer}>
-        <div className={styles.objectsContainers}>
+        <div className={styles.objectsContainer}>
           <div className={styles.firstObjectContainer}>
-            <div className={styles.halfCircle}>
-              <div className={styles.halfCircleReflection}></div>
-            </div>
+            <div className={styles.circle}></div>
+            <div className={styles.halfCircle}></div>
+            <div className={styles.shadow}></div>
           </div>
         </div>
       </div>
