ef441659cbeee7fe8cf8b8d5044d969d213399ee
Axel Martin <axel.martin@outlook.com>
2024-09-20 19:44:59 +0200

    commit fin de journée

--- src/components/Sections/Hero/V2Hero.tsx
+++ src/components/Sections/Hero/V2Hero.tsx
@@ -2,6 +2,8 @@
 // import styles from './V2Hero.module.scss';
 // import Image from 'next/image';
 // import ampoule from '../../../../public/ampoule.jpg';
+// import { barlow } from '@/styles/font';
+// import Button from '@/components/Buttons/Button';
 
 // export default function V2Hero() {
 //   return (
@@ -9,6 +11,27 @@
 //       <div className={styles.heroMain}>
 //         <div className={styles.overflowControl}>
 //           <div className={styles.skewedContainer}></div>
+//           <div className={styles.contentContainer}>
+//             <h1 className={`${styles.title} ${barlow.className}`}>
+//               <span className={styles.firstWord}>Optimisez </span>
+//               votre{' '}
+//               <span className={styles.highlight}>
+//                 gestion opérationnelle
+//               </span> & <span className={styles.highlight}>Financière</span>
+//             </h1>
+//             <p className={`${styles.description} ${barlow.className}`}>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim
+//               ad minim veniam. Lorem ipsum dolor sit amet consectetur,
+//               adipisicing elit. Esse reprehenderit eos qui dolore! Tempore
+//               assumenda voluptas consequuntur officiis quod quam iste, illum
+//               nesciunt minus sequi ullam cum veritatis quasi placeat!
+//             </p>
+
+//             <div className={styles.buttonsContainer}>
+//               <Button label="Mes services" variant="primary" />
+//               <Button label="Audit gratuit" variant="secondary" />
+//             </div>
+//           </div>
 //         </div>
 
 //         <div className={styles.backgroundContainer}>
@@ -33,17 +56,39 @@ import React from 'react';
 import styles from './V2Hero.module.scss';
 import Image from 'next/image';
 import ampoule from '../../../../public/ampoule.jpg';
+import { barlow } from '@/styles/font';
+import Button from '@/components/Buttons/Button';
 
 export default function V2Hero() {
   return (
     <div className={styles.heroMainContainer}>
-      <div className={styles.heroMain}>
+      <div className={styles.mainContainer}>
         <div className={styles.overflowControl}>
           <div className={styles.skewedContainer}></div>
-          <div className={styles.contentContainer}></div>
+          <div className={styles.contentContainer}>
+            <h1 className={`${styles.title} ${barlow.className}`}>
+              <span className={styles.firstWord}>Optimisez </span>
+              votre{' '}
+              <span className={styles.highlight}>
+                gestion opérationnelle
+              </span> & <span className={styles.highlight}>Financière</span>
+            </h1>
+            <p className={`${styles.description} ${barlow.className}`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim
+              ad minim veniam. Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Esse reprehenderit eos qui dolore! Tempore
+              assumenda voluptas consequuntur officiis quod quam iste, illum
+              nesciunt minus sequi ullam cum veritatis quasi placeat!
+            </p>
+
+            <div className={styles.buttonsContainer}>
+              <Button label="Mes services" variant="primary" />
+              <Button label="Audit gratuit" variant="secondary" />
+            </div>
+          </div>
         </div>
 
-        <div className={styles.backgroundContainer}>
+        <div className={styles.backgroundImageContainer}>
           <div className={styles.imageContainer}>
             <Image
               className={styles.image}
