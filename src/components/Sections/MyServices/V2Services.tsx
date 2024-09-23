ec7e50e3ae1d30ab1a33580d0e50cb92df7a5038
Axel Martin <axel.martin@outlook.com>
2024-09-23 19:17:46 +0200

    commit fin de journée

--- src/components/Sections/MyServices/V2Services.tsx
+++ src/components/Sections/MyServices/V2Services.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './V2Services.module.scss';
 
 export default function V2Services() {
-  return <div>V2Services</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.backgroundRibbon}></div>
+    </div>
+  );
 }
