49a3f71c7d5671a2bb27113e2040fc24cf3ea633
Axel Martin <axel.martin@outlook.com>
2024-09-24 18:31:11 +0200

    commit fin de journée

--- src/components/Sections/MyServices/V2Services.tsx
+++ src/components/Sections/MyServices/V2Services.tsx
@@ -4,7 +4,16 @@ import styles from './V2Services.module.scss';
 export default function V2Services() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.backgroundRibbon}></div>
+      <div className={styles.mobileMainContainer}>
+        <div className={styles.cardsContainer}>
+          <div className={styles.card1}>
+            <div className={styles.contentCard}></div>
+            <div className={styles.backgroundRibbon}>
+              <div className={styles.coloredRibbon}></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
