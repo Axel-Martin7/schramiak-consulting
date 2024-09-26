e40a467836fd323fe15cd25723b66782d85f190c
Axel Martin <axel.martin@outlook.com>
2024-09-26 18:00:49 +0200

    Commit fin de journée

--- src/components/Sections/Contact/V2Contact.tsx
+++ src/components/Sections/Contact/V2Contact.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './V2Contact.module.scss';
+import { barlow } from '@/styles/font';
 
 export default function V2Contact() {
-  return <div>V2Contact</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.sectionTitleContainer}>
+        <h2 className={`${styles.title} ${barlow.className}`}>
+          Rencontrons-nous
+        </h2>
+        <div className={styles.titleSeparator}></div>
+      </div>
+    </div>
+  );
 }
