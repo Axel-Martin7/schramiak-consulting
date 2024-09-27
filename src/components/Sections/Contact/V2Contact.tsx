e38ff08fd37905ce23e10306103c06e41d41d0fd
Axel Martin <axel.martin@outlook.com>
2024-09-27 16:00:52 +0200

    created structure for mobile and desktop versions of contact component

--- src/components/Sections/Contact/V2Contact.tsx
+++ src/components/Sections/Contact/V2Contact.tsx
@@ -11,6 +11,42 @@ export default function V2Contact() {
         </h2>
         <div className={styles.titleSeparator}></div>
       </div>
+
+      <div className={styles.contactContainer}>
+        <div className={styles.emptyCarvedBackground}></div>
+
+        <div className={styles.contactItemsContainer}>
+          <div className={styles.item1}>
+            <div className={styles.circleContainer}></div>
+            <p className={`${styles.itemInformation} ${barlow.className}`}>
+              1208 Genève
+            </p>
+          </div>
+
+          <div className={styles.item2}>
+            <div className={styles.circleContainer}></div>
+            <p className={`${styles.itemInformation} ${barlow.className}`}>
+              steven.schramiak@icloud.com
+            </p>
+          </div>
+
+          <div className={styles.item3}>
+            <div className={styles.circleContainer}></div>
+            <p className={`${styles.itemInformation} ${barlow.className}`}>
+              Linkedin
+            </p>
+          </div>
+
+          <div className={styles.item4}>
+            <div className={styles.circleContainer}></div>
+            <p className={`${styles.itemInformation} ${barlow.className}`}>
+              +41.78.204.70.78
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.contactInformationsContainer}></div>
+      </div>
     </div>
   );
 }
