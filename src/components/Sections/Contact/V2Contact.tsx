36d96811a909ecf0846d603a3c3c670affa4abdf
Axel Martin <axel.martin@outlook.com>
2024-09-27 17:54:46 +0200

    Added icons to circle items

--- src/components/Sections/Contact/V2Contact.tsx
+++ src/components/Sections/Contact/V2Contact.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './V2Contact.module.scss';
+import Image from 'next/image';
 import { barlow } from '@/styles/font';
+
+import localisation from '../../../../public/icons/place-marker-icon.svg';
+import mail from '../../../../public/icons/mail-icon.svg';
+import linkedin from '../../../../public/icons/linkedin-icon.svg';
+import phone from '../../../../public/icons/whatsapp-icon.svg';
 
 export default function V2Contact() {
   return (
@@ -19,7 +25,15 @@ export default function V2Contact() {
           {/* Item 1 : localisation --> */}
           <div className={styles.item1}>
             <div className={styles.circleBorder}>
-              <div className={styles.circle}></div>
+              <div className={styles.circle}>
+                <Image
+                  className={styles.icon}
+                  src={localisation}
+                  width={40}
+                  height={40}
+                  alt="Email icon"
+                />
+              </div>
             </div>
 
             <p className={`${styles.itemInformation} ${barlow.className}`}>
@@ -30,7 +44,15 @@ export default function V2Contact() {
           {/* Item 2 : adresse mail --> */}
           <div className={styles.item2}>
             <div className={styles.circleBorder}>
-              <div className={styles.circle}></div>
+              <div className={styles.circle}>
+                <Image
+                  className={styles.icon}
+                  src={mail}
+                  width={40}
+                  height={40}
+                  alt="Email icon"
+                />
+              </div>
             </div>
 
             <p className={`${styles.itemInformation} ${barlow.className}`}>
@@ -41,7 +63,15 @@ export default function V2Contact() {
           {/* Item 3 : linkedin --> */}
           <div className={styles.item3}>
             <div className={styles.circleBorder}>
-              <div className={styles.circle}></div>
+              <div className={styles.circle}>
+                <Image
+                  className={styles.icon}
+                  src={linkedin}
+                  width={40}
+                  height={40}
+                  alt="Email icon"
+                />
+              </div>
             </div>
             <p className={`${styles.itemInformation} ${barlow.className}`}>
               Linkedin
@@ -51,7 +81,15 @@ export default function V2Contact() {
           {/* Item 4 : whatsapp --> */}
           <div className={styles.item4}>
             <div className={styles.circleBorder}>
-              <div className={styles.circle}></div>
+              <div className={styles.circle}>
+                <Image
+                  className={styles.icon}
+                  src={phone}
+                  width={40}
+                  height={40}
+                  alt="Email icon"
+                />
+              </div>
             </div>
             <p className={`${styles.itemInformation} ${barlow.className}`}>
               +41.78.204.70.78
