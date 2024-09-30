b1dde5516be6abe9c3018dd293ebf8086b7c68c8
Axel Martin <axel.martin@outlook.com>
2024-09-30 14:12:14 +0200

    Created form for v2Contact component

--- src/components/Sections/Contact/V2Contact.tsx
+++ src/components/Sections/Contact/V2Contact.tsx
@@ -97,10 +97,49 @@ export default function V2Contact() {
           </div>
         </div>
 
-        <div className={styles.contactInformationsContainer}>
-          <h3 className={`${styles.auditTitle} ${barlow.className}`}>
-            Réservez un audit gratuit de vos besoins dès maintenant !
-          </h3>
+        <div className={styles.informationsCarvedBackground}>
+          <div className={styles.contactInformationsContainer}>
+            <h3 className={`${styles.auditTitle} ${barlow.className}`}>
+              Réservez un{' '}
+              <span className={styles.highlight}>audit gratuit</span> de vos
+              besoins dès maintenant !
+            </h3>
+
+            <p className={styles.auditDescription}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+              optio corporis in totam excepturi, qui minima repudiandae voluptas
+              vitae veniam temporibus odit ad. Magnam sapiente reprehenderit
+              suscipit! Numquam, fugiat ut!
+            </p>
+
+            <div className={styles.formContainer}>
+              <form className={styles.contactForm}>
+                <div className={styles.formGroup}>
+                  <label htmlFor="nom">Nom</label>
+                  <input type="text" id="nom" name="nom" required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="prenom">Prénom</label>
+                  <input type="text" id="prenom" name="prenom" required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="email">Adresse Mail</label>
+                  <input type="email" id="email" name="email" required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="compagnie">Compagnie (Optionnel)</label>
+                  <input type="text" id="compagnie" name="compagnie" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="message">Message</label>
+                  <textarea id="message" name="message" required></textarea>
+                </div>
+                <button type="submit" className={styles.submitButton}>
+                  Envoyer
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
