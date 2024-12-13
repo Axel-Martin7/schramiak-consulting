279e2f99ef4699f928135cbb7804694d3e09ea42
Axel Martin <axel.martin@outlook.com>
2024-12-13 10:13:42 +0100

    Adjust grey's color palette

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -3,14 +3,20 @@ import styles from './About.module.scss';
 
 export default function About() {
   return (
+    // <div className={styles.aboutContainer}>
+    //   {/* <div className={styles.titleCard}>About me</div>
+
+    // <div className={styles.imageCard}>IMG</div>
+
+    // <div className={styles.quoteCard}>quote</div>
+
+    // <div className={styles.descriptionCard}>description</div> */}
+    // </div>
+
     <div className={styles.aboutContainer}>
-      <div className={styles.titleCard}>About me</div>
+      <div className={styles.aboutCard}></div>
 
-      <div className={styles.imageCard}>IMG</div>
-
-      <div className={styles.quoteCard}>quote</div>
-
-      <div className={styles.descriptionCard}>description</div>
+      {/* <div className={styles.shadow}></div> */}
     </div>
   );
 }
