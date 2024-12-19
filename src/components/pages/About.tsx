fb5c2a8349dd84fefb1b58b07ac90711df0d8a75
Axel Martin <axel.martin@outlook.com>
2024-12-19 15:44:35 +0100

    Create new style for About Component

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -2,42 +2,9 @@ import React from 'react';
 import styles from './About.module.scss';
 import ProfilPictureShape from './ProfilPictureShape';
 import { barlow } from '@/styles/font';
-import Photo from './Photo';
 
 export default function About() {
   return (
-    // <div className={styles.aboutContainer}>
-    //   {/* <div className={styles.titleCard}>About me</div>
-
-    // <div className={styles.imageCard}>IMG</div>
-
-    // <div className={styles.quoteCard}>quote</div>
-
-    // <div className={styles.descriptionCard}>description</div> */}
-    // </div>
-
-    // --------------------------------------------------------------
-
-    // <div className={styles.aboutContainer}>
-    //   <div className={styles.aboutCard}>
-    //     <div className={styles.titleContainer}>
-    //       <h2 className={`${styles.sectionTitle} ${barlow.className}`}>
-    //         About me
-    //       </h2>
-    //       <div className={styles.titleSeparator}></div>
-    //     </div>
-    //     <div className={styles.imageContainer}>
-    //       <ProfilPictureShape />
-    //     </div>
-    //     <div className={styles.quoteContainer}></div>
-    //     <div className={styles.descriptionContainer}></div>
-    //   </div>
-
-    //   <div className={styles.cardShadow}></div>
-
-    //   <div className={styles.cardReflect}></div>
-    // </div>
-
     <div className={styles.aboutContainer}>
       <div className={styles.aboutCard}>
         <section className={styles.titleSection}>
@@ -46,30 +13,58 @@ export default function About() {
         </section>
 
         <section className={styles.imageSection}>
-          {/* <div className={styles.imageContainer}></div> */}
           <ProfilPictureShape />
         </section>
 
         <section className={styles.quoteSection}>
-          <p>
+          <blockquote className={`${styles.quote} ${barlow.className}`}>
             Chaque projet passe par l etablissement d une relation de confiance
-            et d un systeme de collaboration, en vue de contribuer a votre
-            reussite ainsi qu a celle de vos clients.
-          </p>
+            et d un systeme de collaboration, <br></br>en vue de contribuer a
+            votre reussite ainsi qu a celle de vos clients.
+          </blockquote>
         </section>
 
         <section className={styles.presentationSection}>
-          <article className={styles.descriptionContainer}>
+          <article className={styles.descriptionsContainer}>
             <p className={styles.description}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium eveniet odit provident possimus, praesentium maiores.
-              Aut repudiandae fuga explicabo voluptatibus sapiente!
-              Exercitationem id nisi blanditiis repellendus tempore tenetur non
-              incidunt.
+              Je suis Steven Schramiak, <br></br> consultant en management
+              financier avec une forte polyvalence et un esprit entrepreneurial.
+            </p>
+            <p className={styles.description}>
+              {' '}
+              Avec plus de 10 ans d expérience dans le domaine, j ai aidé de
+              nombreuses entreprises à optimiser leur gestion financière et
+              opérationnelle.
+            </p>
+            <p className={styles.description}>
+              {' '}
+              Mon parcours professionnel m a permis de développer une expertise
+              pointue en controlling, gestion de projet et création de startups.
+            </p>
+            <p className={styles.description}>
+              Ayant collaboré avec des PME et des start-ups de divers secteurs,{' '}
+              <br></br>j ai acquis une riche expérience dans des industries
+              telles que la construction, les services aux entreprises, le
+              commerce de détail et la vente en ligne.
+            </p>
+            <p className={styles.description}>
+              {' '}
+              Mon approche est centrée sur la mise en place de solutions
+              innovantes et adaptées aux besoins spécifiques de chaque
+              entreprise, tout en assurant une transparence et une
+              confidentialité totale.
             </p>
           </article>
+
+          <div className={styles.buttonContainer}>
+            <button className={styles.button}>Button</button>
+          </div>
         </section>
       </div>
+
+      <div className={styles.cardShadow}></div>
+
+      <div className={styles.cardReflect}></div>
     </div>
   );
 }
