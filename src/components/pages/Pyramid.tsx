d380f267bdb5216ec6cbc6552dbf304b536f4a39
Axel Martin <axel.martin@outlook.com>
2024-12-28 13:49:07 +0100

    Create shadows structure for floors elements into Pyramid component

--- src/components/pages/Pyramid.tsx
+++ src/components/pages/Pyramid.tsx
@@ -6,18 +6,34 @@ export default function Pyramid() {
     <div className={styles.pyramidContainer}>
       <div className={styles.repere}></div>
       <div className={styles.base}>
-        <div className={styles.firstFloor}></div>
-        <div className={styles.secondFloor}></div>
-        <div className={styles.thirdFloor}></div>
-        <div className={styles.fourthFloor}></div>
+        <div className={styles.firstFloor}>
+          <div className={styles.darkSide}></div>
+        </div>
+        <div className={styles.secondFloor}>
+          <div className={styles.darkSide}></div>
+        </div>
+        <div className={styles.thirdFloor}>
+          <div className={styles.darkSide}></div>
+        </div>
+        <div className={styles.fourthFloor}>
+          <div className={styles.darkSide}></div>
+        </div>
         <div className={styles.hatRepere}></div>
 
         <div className={styles.firstFloorCeiling}></div>
         <div className={styles.secondFloorCeiling}></div>
         <div className={styles.thirdFloorCeiling}></div>
         <div className={styles.fourthFloorCeiling}></div>
-        <div className={styles.hat}></div>
+        <div className={styles.hat}>
+          <div className={styles.darkSide}></div>
+        </div>
       </div>
+
+      <div className={styles.shadowFirstFloor}></div>
+      <div className={styles.shadowSecondFloor}></div>
+      <div className={styles.shadowThirdFloor}></div>
+      <div className={styles.shadowFourthFloor}></div>
+      <div className={styles.shadowHat}></div>
     </div>
   );
 }
