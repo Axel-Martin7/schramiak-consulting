342dee54d5c82676ecc39c8c89b7ce0195e876d9
Axel Martin <axel.martin@outlook.com>
2025-01-03 15:51:23 +0100

    Create style for Hero components buttons

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -51,6 +51,98 @@ export default function Hero() {
     //   </div>
     // </div>
 
-    <div className={styles.heroRibbon}>Hero</div>
+    // <div className={styles.heroRibbon}>
+    //   <div className={styles.skewedContainer}></div>
+    //   <div className={styles.heroContentContainer}>
+    //     <section className={styles.informationsSection}>
+    //       <h2 className={`${styles.heroTitleContainer} ${barlow.className} `}>
+    //         <strong className={styles.firstWord}>Optimisez</strong>
+    //         <small className={styles.smallWord}>votre </small>
+    //         <strong className={styles.highlightedWords}>
+    //           Gestion opérationnelle
+    //         </strong>
+    //         <small className={styles.smallWord}> & </small>
+    //         <strong className={styles.highlightWords}>Financière</strong>
+    //       </h2>
+
+    //       <p className={`${styles.description} ${barlow.className}`}>
+    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad
+    //         minim veniam. Lorem ipsum dolor sit amet consectetur, adipisicing
+    //         elit. Esse reprehenderit eos qui dolore! Tempore assumenda voluptas
+    //         consequuntur officiis quod quam iste, illum nesciunt minus sequi
+    //         ullam cum veritatis quasi placeat!
+    //       </p>
+
+    //       <div className={styles.buttonsContainer}>
+    //         <button className={styles.button}>Découvrez mes services</button>
+    //         <button className={styles.primaryButton}>
+    //           Obtenez un audit gratuit
+    //         </button>
+    //       </div>
+    //     </section>
+    //   </div>
+    //   <section className={styles.backgroundImageSection}>
+    //     <div className={styles.imageContainer}>
+    //       <Image
+    //         className={styles.image}
+    //         alt="image ampoule"
+    //         src={ampoule}
+    //         placeholder="blur"
+    //         quality={100}
+    //         fill
+    //         sizes="100vw"
+    //       />
+    //     </div>
+    //   </section>
+    // </div>
+
+    <div className={styles.heroRibbon}>
+      <div className={styles.skewedContainer}></div>
+
+      <div className={styles.heroContentContainer}>
+        <section className={styles.informationsSection}>
+          <h2 className={`${styles.heroTitleContainer} ${barlow.className} `}>
+            <strong className={styles.firstWord}>Optimisez</strong>
+            <small className={styles.smallWord}>votre </small>
+            <strong className={styles.highlightWords}>
+              Gestion opérationnelle
+            </strong>
+            <small className={styles.smallWord}> & </small>
+            <strong className={styles.highlightWords}>Financière</strong>
+          </h2>
+
+          <p className={`${styles.description} ${barlow.className}`}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad
+            minim veniam. Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Esse reprehenderit eos qui dolore! Tempore assumenda voluptas
+            consequuntur officiis quod quam iste, illum nesciunt minus sequi
+            ullam cum veritatis quasi placeat!
+          </p>
+
+          <div className={styles.buttonsContainer}>
+            <button className={`${styles.secondaryButton} ${barlow.className}`}>
+              Découvrez mes services
+            </button>
+            <button className={`${styles.primaryButton} ${barlow.className}`}>
+              Obtenez un audit gratuit
+            </button>
+          </div>
+        </section>
+
+        <section className={styles.backgroundImageSection}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.image}
+              alt="image ampoule"
+              src={ampoule}
+              placeholder="blur"
+              quality={100}
+              fill
+              sizes="100vw"
+            />
+          </div>
+        </section>
+      </div>
+    </div>
   );
 }
