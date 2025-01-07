c48c8ead873c5af7063d7ed79b6bc2fd4c1e5333
Axel Martin <axel.martin@outlook.com>
2025-01-07 19:10:09 +0100

    Create Aboutv2 component

--- src/components/pages/AboutV2.tsx
+++ src/components/pages/AboutV2.tsx
@@ -6,28 +6,86 @@ import steven from '../../../public/Steven-nobg.png';
 
 export default function AboutV2() {
   return (
+    //  <div className={styles.aboutContainer}>
+    //    <div className={styles.sectionTitle}>
+    //      <h2 className={`${styles.title} ${barlow.className}`}>About me</h2>
+    //      <div className={styles.titleSeparator}></div>
+    //    </div>
+
+    //    <div className={styles.aboutCardFrame}>
+    //      <div className={styles.aboutCard}>
+    //        <div className={styles.imageSection}>
+    //          <div className={styles.imageFrame}>
+    //            <div className={styles.imageContainer}>
+    //              <Image
+    //                className={styles.image}
+    //                alt="photo de Steven Schramiak"
+    //                src={steven}
+    //                placeholder="blur"
+    //                quality={100}
+    //                fill
+    //                sizes="100vw"
+    //              />
+    //            </div>
+    //          </div>
+    //        </div>
+
+    //        <div className={styles.presentationSection}>
+    //          <div className={styles.quoteSection}>
+    //            <p>
+    //              Chaque projet passe par l etablissement d une relation de
+    //              confiance et d un systeme de collaboration, en vue de contribuer
+    //              a votre reussite ainsi qu a celle de vos clients.
+    //            </p>
+    //          </div>
+
+    //          <div className={styles.nameSection}>
+    //            <div className={styles.separator}></div>
+    //            <h3 className={styles.name}>Steven Schramiak</h3>
+    //            <p className={styles.role}>Consultant financier</p>
+    //          </div>
+
+    //          <div className={styles.descriptionsContainer}>
+    //            <p className={styles.description}>
+    //              Je suis Steven Schramiak, consultant en management financier
+    //              avec une forte polyvalence et un esprit entrepreneurial. Avec
+    //              plus de 10 ans d expérience dans le domaine, j ai aidé de
+    //              nombreuses entreprises à optimiser leur gestion financière et
+    //              opérationnelle. Mon parcours professionnel m a permis de
+    //              développer une expertise pointue en controlling, gestion de
+    //              projet et création de startups.
+    //            </p>
+    //            <p className={styles.description}>
+    //              Ayant collaboré avec des PME et des start-ups de divers
+    //              secteurs, j ai acquis une riche expérience dans des industries
+    //              telles que la construction, les services aux entreprises, le
+    //              commerce de détail et la vente en ligne. Mon approche est
+    //              centrée sur la mise en place de solutions innovantes et adaptées
+    //              aux besoins spécifiques de chaque entreprise, tout en assurant
+    //              une transparence et une confidentialité totale.
+    //            </p>
+    //          </div>
+
+    //          <div className={styles.buttonContainer}>
+    //            <button className={`${styles.button} ${barlow.className}`}>
+    //              Découvrez mon parcours
+    //            </button>
+    //          </div>
+    //        </div>
+    //      </div>
+    //    </div>
+    //  </div>
+
     <div className={styles.aboutContainer}>
       <div className={styles.sectionTitle}>
         <h2 className={`${styles.title} ${barlow.className}`}>About me</h2>
         <div className={styles.titleSeparator}></div>
       </div>
 
-      <div className={styles.aboutCard}>
-        <div className={styles.descriptionsContainer}></div>
+      <div className={styles.aboutCardFrame}>
+        {/* <div className={styles.aboutCard}></div> */}
 
-        <div className={styles.imageContainer}>
-          <div className={styles.image}>
-            <Image
-              className={styles.image}
-              alt="photo de Steven Schramiak"
-              src={steven}
-              placeholder="blur"
-              quality={100}
-              fill
-              sizes="100vw"
-            />
-          </div>
-        </div>
+        <div className={styles.aboutCard}></div>
       </div>
     </div>
   );
