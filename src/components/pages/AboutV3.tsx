fd0942d7fc8f16ad3619acdf944786b1d126979b
Axel Martin <axel.martin@outlook.com>
2025-01-14 10:43:12 +0100

    Card structure done for AboutV3 Component

--- src/components/pages/AboutV3.tsx
+++ src/components/pages/AboutV3.tsx
@@ -44,6 +44,42 @@ export default function AboutV3() {
     //         </blockquote>
 
     //         <div className={styles.separator}></div>
+
+    //         <div className={styles.informationsContainer}>
+    //           <div className={styles.titlesContainer}>
+    //             <h3 className={styles.name}>
+    //               Steven <strong className={styles.lastName}>Schramiak</strong>
+    //             </h3>
+    //             <h4 className={styles.role}>Consultant financier</h4>
+    //           </div>
+
+    //           <div className={styles.descriptionsContainer}>
+    //             <p className={styles.description}>
+    //               Je suis Steven Schramiak, consultant en management financier
+    //               avec une forte polyvalence et un esprit entrepreneurial. Avec
+    //               plus de 10 ans d expérience dans le domaine, j ai aidé de
+    //               nombreuses entreprises à optimiser leur gestion financière et
+    //               opérationnelle. Mon parcours professionnel m a permis de
+    //               développer une expertise pointue en controlling, gestion de
+    //               projet et création de startups.
+    //             </p>
+    //             <p className={styles.description}>
+    //               Ayant collaboré avec des PME et des start-ups de divers
+    //               secteurs, j ai acquis une riche expérience dans des industries
+    //               telles que la construction, les services aux entreprises, le
+    //               commerce de détail et la vente en ligne. Mon approche est
+    //               centrée sur la mise en place de solutions innovantes et
+    //               adaptées aux besoins spécifiques de chaque entreprise, tout en
+    //               assurant une transparence et une confidentialité totale.
+    //             </p>
+    //           </div>
+
+    //           <div className={styles.buttonContainer}>
+    //             <button className={`${styles.button} ${barlow.className}`}>
+    //               Découvrez mon parcours
+    //             </button>
+    //           </div>
+    //         </div>
     //       </div>
     //     </div>
     //   </div>
@@ -79,51 +115,7 @@ export default function AboutV3() {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.presentationSection}>
-            <blockquote className={styles.quote}>
-              Chaque projet passe par l&apos;établissement d&apos;une relation
-              de confiance et d&apos;un système de collaboration, en vue de
-              contribuer à votre réussite ainsi qu&apos;à celle de vos clients.
-            </blockquote>
-
-            <div className={styles.separator}></div>
-
-            <div className={styles.informationsContainer}>
-              <div className={styles.titlesContainer}>
-                <h3 className={styles.name}>
-                  Steven <strong className={styles.lastName}>Schramiak</strong>
-                </h3>
-                <h4 className={styles.role}>Consultant financier</h4>
-              </div>
-
-              <div className={styles.descriptionsContainer}>
-                <p className={styles.description}>
-                  Je suis Steven Schramiak, consultant en management financier
-                  avec une forte polyvalence et un esprit entrepreneurial. Avec
-                  plus de 10 ans d expérience dans le domaine, j ai aidé de
-                  nombreuses entreprises à optimiser leur gestion financière et
-                  opérationnelle. Mon parcours professionnel m a permis de
-                  développer une expertise pointue en controlling, gestion de
-                  projet et création de startups.
-                </p>
-                <p className={styles.description}>
-                  Ayant collaboré avec des PME et des start-ups de divers
-                  secteurs, j ai acquis une riche expérience dans des industries
-                  telles que la construction, les services aux entreprises, le
-                  commerce de détail et la vente en ligne. Mon approche est
-                  centrée sur la mise en place de solutions innovantes et
-                  adaptées aux besoins spécifiques de chaque entreprise, tout en
-                  assurant une transparence et une confidentialité totale.
-                </p>
-              </div>
-
-              <div className={styles.buttonContainer}>
-                <button className={`${styles.button} ${barlow.className}`}>
-                  Découvrez mon parcours
-                </button>
-              </div>
-            </div>
-          </div>
+          <div className={styles.presentationSection}></div>
         </div>
       </div>
     </div>
