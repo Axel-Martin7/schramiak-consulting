2fd0960bcdf4a4a8cfc3262a290080b798c7a9c2
Axel Martin <axel.martin@outlook.com>
2025-03-20 18:44:29 +0100

    Add ssc logo and style on Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,10 +2,53 @@ import React from 'react';
 import styles from './Footer.module.scss';
 import Image from 'next/image';
 import logo from '../../../public/ssc-logo.svg';
+import logo2 from '../../../public/ssc-logo2.svg';
 import { barlow } from '@/styles/font';
 
 export default function Footer() {
   return (
+    // <footer className={styles.footer}>
+    //   <section className={styles.thanksSection}>
+    //     <div className={styles.contentContainer}>
+    //       <span className={styles.separator}></span>
+    //       <div className={styles.thanksMessageContainer}>
+    //         <h3 className={`${styles.thanksMessage} ${barlow.className}`}>
+    //           Merci de votre visite !
+    //         </h3>
+    //         <p className={styles.signature}>S.Schramiak .</p>
+    //       </div>
+    //     </div>
+
+    //     {/*---------- Layouts -----------*/}
+    //     <div className={styles.lowerLayoutWrapper}>
+    //       <div className={styles.lowerLayout}></div>
+    //     </div>
+    //     <div className={styles.upperLayoutWrapper}>
+    //       <div className={styles.upperLayout}></div>
+    //     </div>
+    //   </section>
+
+    //   <section className={styles.carvedSection}>
+    //     <div className={styles.logoContainer}>
+    //       <Image
+    //         className={styles.logo2}
+    //         alt="Schramiak consulting logo"
+    //         src={logo2}
+    //         width={80}
+    //         height={80}
+    //       ></Image>
+
+    //       <small className={styles.logoCaption}>Consulting</small>
+    //     </div>
+
+    //     <div className={styles.nav}>
+    //       <div className={styles.item}>HOME</div>
+    //       <div className={styles.item}>ABOUT</div>
+    //       <div className={styles.item}>CONTACT</div>
+    //     </div>
+    //   </section>
+    // </footer>
+
     <footer className={styles.footer}>
       <section className={styles.thanksSection}>
         <div className={styles.contentContainer}>
@@ -30,13 +73,35 @@ export default function Footer() {
       <section className={styles.carvedSection}>
         <div className={styles.logoContainer}>
           <Image
-            className={styles.image}
+            className={styles.logo2}
             alt="Schramiak consulting logo"
-            src={logo}
-            width={64}
-            height={64}
+            src={logo2}
+            width={80}
+            height={80}
           ></Image>
+
+          <small className={styles.logoCaption}>Consulting</small>
         </div>
+
+        <div className={styles.navContainer}>
+          <nav className={styles.nav}>
+            <div className={styles.item}>HOME</div>
+            <div className={styles.item}>ABOUT</div>
+            <div className={styles.item}>PARCOUR</div>
+            <div className={styles.item}>SERVICES</div>
+            <div className={styles.item}>REALISATIONS</div>
+            <div className={styles.item}>CONTACT</div>
+          </nav>
+
+          <div className={styles.social}>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+          </div>
+        </div>
+
+        <p className={styles.small}>Developped by Axel Martin</p>
       </section>
     </footer>
   );
