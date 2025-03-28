0de567969f1ee128f911f59f3124c0e22cebf0f5
Axel Martin <axel.martin@outlook.com>
2025-03-28 18:10:23 +0100

    Create a new structure for mobile version of Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,38 +8,42 @@ import Logo from '../common/Logo';
 
 export default function Header() {
   return (
-    // <header className={styles.header}>
-    //   <div className={styles.headerContentContainer}>
-    //     <section className={styles.logoBtnContainer}>
-    //       <Link href={'/'}>
-    //         <p>Logo Home</p>
-    //       </Link>
-    //     </section>
+    //  <header className={styles.header}>
+    //    <div className={styles.headerContentContainer}>
+    //      <section className={styles.logoBtnContainer}>
+    //        <Link href={'/'}>
+    //          <p>Logo Home</p>
+    //        </Link>
+    //      </section>
 
-    //     {/* Burger Menu visible uniquement sur mobile */}
-    //     <section className={styles.burgerMenuMobile}>
-    //       <BurgerMenu />
-    //     </section>
+    //      {/* Burger Menu visible uniquement sur mobile */}
+    //      <section className={styles.burgerMenuMobile}>
+    //        <BurgerMenu />
+    //      </section>
 
-    //     {/* Navbar desktop visible uniquement sur les grands écrans */}
-    //     <section className={styles.navBarDesktop}>
-    //       <Navigation />
-    //     </section>
+    //      {/* Navbar desktop visible uniquement sur les grands écrans */}
+    //      <section className={styles.navBarDesktop}>
+    //        <Navigation />
+    //      </section>
 
-    //     <section className={styles.languageSwitcherContainer}>
-    //       <LocaleSwitcher />
-    //     </section>
-    //   </div>
-    // </header>
+    //      <section className={styles.languageSwitcherContainer}>
+    //        <LocaleSwitcher />
+    //      </section>
+    //    </div>
+    //  </header>
 
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <Logo variant="headerLogo" />
+      <div className={styles.contentContainer}>
+        <div className={styles.logoContainer}>
+          <div className={styles.logo}></div>
+        </div>
+
+        <div className={styles.burgerMenu}></div>
+
+        <div className={styles.switcherContainer}>
+          <div className={styles.switcher}></div>
+        </div>
       </div>
-
-      <div className={styles.burgerContainer}>Menu</div>
-
-      <div className={styles.switcherContainer}>Switch</div>
     </header>
   );
 }
