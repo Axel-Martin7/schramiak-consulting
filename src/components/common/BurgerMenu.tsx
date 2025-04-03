965724ed1cebac9413e458d2a7a2fa9b79904c60
Axel Martin <axel.martin@outlook.com>
2025-04-03 19:52:23 +0200

    Created shadow style and button structure for BurgerMenu component

--- src/components/common/BurgerMenu.tsx
+++ src/components/common/BurgerMenu.tsx
@@ -3,13 +3,8 @@ import styles from './BurgerMenu.module.scss';
 
 export default function BurgerMenu() {
   return (
-    <label className={styles.burgerWrap} htmlFor="button">
-      <input type="checkbox" aria-label="Bluetooth" id="button" />
-      <button className={styles.button}>
-        <div className={styles.corner}></div>
-        <div className={styles.inner}></div>
-      </button>
-      <div className={styles.bg}></div>
-    </label>
+    <div className={styles.burgerContainer}>
+      <button className={styles.burgerButton}></button>
+    </div>
   );
 }
