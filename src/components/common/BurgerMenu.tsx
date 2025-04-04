8fd0d87214f98e0cd7f4aa2ab7fa2566a9f6d47d
Axel Martin <axel.martin@outlook.com>
2025-04-04 18:47:10 +0200

    Adjust style to burgerButton and added animations for opening and closing

--- src/components/common/BurgerMenu.tsx
+++ src/components/common/BurgerMenu.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import styles from './BurgerMenu.module.scss';
 
 export default function BurgerMenu() {
+  const [pressed, setPressed] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setPressed(true);
+    setTimeout(() => {
+      setPressed(false);
+      setIsOpen((prev) => !prev);
+    }, 200);
+  };
+
   return (
     <div className={styles.burgerContainer}>
-      <button className={styles.burgerButton}></button>
+      <button
+        className={`${styles.burgerButton} ${pressed ? styles.pressed : ''}`}
+        onClick={handleClick}
+      >
+        <div
+          className={`${styles.burgerLinesContainer} ${
+            isOpen ? styles.open : ''
+          }`}
+        >
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+        </div>
+      </button>
     </div>
   );
 }
