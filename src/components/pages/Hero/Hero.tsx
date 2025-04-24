e7408d398ac5f8a4e6ba0d3ca00671ddaf813a80
Axel Martin <axel.martin@outlook.com>
2025-04-24 17:46:19 +0200

    Adjust style for Hero component

--- src/components/pages/Hero/Hero.tsx
+++ src/components/pages/Hero/Hero.tsx
@@ -3,73 +3,24 @@ import styles from './Hero.module.scss';
 import Image from 'next/image';
 import ampoule from '../../../../public/ampoule.jpg';
 import { barlow } from '@/styles/font';
-import Button from '../../common/Button';
 import CtaButton from '@/components/common/CtaButton';
 
 export default function Hero() {
   return (
-    // <div className={styles.heroRibbon}>
-    //   <div className={styles.skewedContainer}></div>
-
-    //   <div className={styles.heroContentContainer}>
-    //     <section className={styles.informationsSection}>
-    //       <h2 className={`${styles.heroTitleContainer} ${barlow.className} `}>
-    //         <strong className={styles.firstWord}>Optimisez</strong>
-    //         <small className={styles.smallWord}>votre </small>
-    //         <strong className={styles.highlightWords}>
-    //           Gestion opérationnelle
-    //         </strong>
-    //         <small className={styles.smallWord}> & </small>
-    //         <strong className={styles.highlightWords}>Financière</strong>
-    //       </h2>
-
-    //       <p className={`${styles.description} ${barlow.className}`}>
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad
-    //         minim veniam. Lorem ipsum dolor sit amet consectetur, adipisicing
-    //         elit. Esse reprehenderit eos qui dolore! Tempore assumenda voluptas
-    //         consequuntur officiis quod quam iste, illum nesciunt minus sequi
-    //         ullam cum veritatis quasi placeat!
-    //       </p>
-
-    //       <div className={styles.buttonsContainer}>
-    //         <Button variant="secondary" className={styles.fullWidthButton}>
-    //           Découvrez mes services
-    //         </Button>
-    //         <Button variant="primary" className={styles.fullWidthButton}>
-    //           Obtenez un audit gratuit
-    //         </Button>
-    //       </div>
-    //     </section>
-
-    //     <section className={styles.backgroundImageSection}>
-    //       <div className={styles.imageContainer}>
-    //         <Image
-    //           className={styles.image}
-    //           alt="image ampoule"
-    //           src={ampoule}
-    //           placeholder="blur"
-    //           quality={100}
-    //           fill
-    //           sizes="(max-width: 768px) 100vw, 50vw"
-    //         />
-    //       </div>
-    //     </section>
-    //   </div>
-    // </div>
-
     <div className={styles.heroRibbon}>
       <div className={styles.skewedContainer}></div>
 
-      <div className={styles.heroContentContainer}>
-        <section className={styles.informationsSection}>
-          <h2 className={`${styles.heroTitleContainer} ${barlow.className} `}>
-            <strong className={styles.firstWord}>Optimisez</strong>
-            <small className={styles.smallWord}>votre </small>
+      <div className={styles.contentContainer}>
+        <section className={styles.ctaSection}>
+          <h2 className={`${styles.heroTitle} ${barlow.className}`}>
+            <strong className={styles.firstWord}>Optimisez </strong>
+            votre{' '}
             <strong className={styles.highlightWords}>
-              Gestion opérationnelle
+              gestion opérationnelle{' '}
             </strong>
-            <small className={styles.smallWord}> & </small>
-            <strong className={styles.highlightWords}>Financière</strong>
+            <span className={styles.newLine}>
+              &<strong className={styles.highlightWords}> financière</strong>
+            </span>
           </h2>
 
           <p className={styles.description}>
@@ -98,7 +49,17 @@ export default function Hero() {
           </div>
         </section>
 
-        <section className={styles.backgroundImageSection}></section>
+        <section className={styles.imageSection}>
+          <Image
+            className={styles.image}
+            alt="Ampoule symbolisant l'optimisation"
+            src={ampoule}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="(min-width: 1280px) 624px, (min-width: 768px) 50vw, 100vh"
+          />
+        </section>
       </div>
     </div>
   );
