f75b520f7a174e4b166d4a77a13b64b3753dc47e
Axel Martin <axel.martin@outlook.com>
2025-05-07 16:23:19 +0200

    Create new structure and style for mobile device of MyServices component

--- src/components/pages/MyServices/MyServices.tsx
+++ src/components/pages/MyServices/MyServices.tsx
@@ -100,6 +100,103 @@ export default function MyServices() {
     //   </ul>
     // </div>
 
+    //*---------------------------------------------------------------------------
+
+    // <div className={styles.servicesContainer}>
+    //   <div className={styles.sectionTitle}>
+    //     <h3 className={`${styles.title} ${barlow.className}`}>Mes services </h3>
+    //     <div className={styles.titleSeparator}></div>
+    //   </div>
+
+    //   <div className={styles.sectionDescription}>
+    //     <p className={styles.description}>
+    //       En tant que consultant, mon rôle sera de mettre en place les
+    //       stratégies et outils nécessaires à l&apos;atteinte de vos objectifs
+    //       ainsi que de ceux de vos clients.
+    //     </p>
+    //     <p className={styles.emphasis}>
+    //       Le plus haut niveau de confidentialité, une transparence et une
+    //       collaboration totale.
+    //     </p>
+    //     <p className={styles.description}>
+    //       Ces engagements nous permettront de gérer au mieux vos ressources et
+    //       de mettre en place les outils adaptés à votre activité, tout en
+    //       accompagnant vos équipes.
+    //     </p>
+    //   </div>
+
+    //   <ul className={styles.servicesList}>
+    //     <li className={styles.service1}>
+    //       <div className={styles.contentWrapper}>
+    //         <div className={styles.bubble1} aria-hidden="true"></div>
+
+    //         <div className={styles.serviceInformationContainer}>
+    //           <h4 className={styles.serviceTitle}>
+    //             CFO / Contrôleur financier
+    //           </h4>
+
+    //           <p className={styles.serviceDescription}>
+    //             Renforcement de votre équipe par une présence à temps partiel
+    //             dans vos locaux.
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </li>
+
+    //     <li className={styles.service2}>
+    //       <div className={styles.contentWrapper}>
+    //         <div className={styles.bubble2} aria-hidden="true"></div>
+
+    //         <div className={styles.serviceInformationContainer}>
+    //           <h4 className={styles.serviceTitle}>
+    //             Rentabilité & Gestion des risques
+    //           </h4>
+    //           <p className={styles.serviceDescription}>
+    //             Analyse de la chaine de valeur et de la rentabilité. <br></br>
+    //             Gestion des risques & amélioration de l&apos;efficacité
+    //             opérationnelle.
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </li>
+
+    //     <li className={styles.service3}>
+    //       <div className={styles.contentWrapper}>
+    //         <div className={styles.bubble3} aria-hidden="true"></div>
+
+    //         <div className={styles.serviceInformationContainer}>
+    //           <h4 className={styles.serviceTitle}>Gestion de projet</h4>
+    //           <p className={styles.serviceDescription}>
+    //             Création, implémentation et déploiement de solutions et
+    //             processus sur mesure. <br></br>
+    //             Développement logiciels métiers.
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </li>
+
+    //     <li className={styles.service4}>
+    //       <div className={styles.contentWrapper}>
+    //         <div className={styles.bubble4} aria-hidden="true"></div>
+
+    //         <div className={styles.serviceInformationContainer}>
+    //           <h4 className={styles.serviceTitle}>
+    //             Planification financière & Business plan
+    //           </h4>
+    //           <p className={styles.serviceDescription}>
+    //             Vision stratégique et financière 3/5 ans. <br></br>
+    //             Accompagnement financements.
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </li>
+    //   </ul>
+
+    //   <div className={styles.decorativePyramidContainer}>
+    //     <Pyramid />
+    //   </div>
+    // </div>
+
     <div className={styles.servicesContainer}>
       <div className={styles.sectionTitle}>
         <h3 className={`${styles.title} ${barlow.className}`}>Mes services </h3>
@@ -130,7 +227,8 @@ export default function MyServices() {
 
             <div className={styles.serviceInformationContainer}>
               <h4 className={styles.serviceTitle}>
-                CFO / Contrôleur financier
+                CFO <small className={styles.small}>/</small> Contrôleur
+                financier
               </h4>
 
               <p className={styles.serviceDescription}>
@@ -147,10 +245,11 @@ export default function MyServices() {
 
             <div className={styles.serviceInformationContainer}>
               <h4 className={styles.serviceTitle}>
-                Rentabilité & Gestion des risques
+                Rentabilité
+                <small className={styles.small}> &</small> Gestion des risques
               </h4>
               <p className={styles.serviceDescription}>
-                Analyse de la chaine de valeur et de la rentabilité. <br></br>
+                Analyse de la chaîne de valeur et de la rentabilité. <br></br>
                 Gestion des risques & amélioration de l&apos;efficacité
                 opérationnelle.
               </p>
@@ -167,7 +266,7 @@ export default function MyServices() {
               <p className={styles.serviceDescription}>
                 Création, implémentation et déploiement de solutions et
                 processus sur mesure. <br></br>
-                Développement logiciels métiers.
+                Développements logiciels métiers.
               </p>
             </div>
           </div>
@@ -179,7 +278,8 @@ export default function MyServices() {
 
             <div className={styles.serviceInformationContainer}>
               <h4 className={styles.serviceTitle}>
-                Planification financière & Business plan
+                Planification financière
+                <small className={styles.small}> &</small> Business plan
               </h4>
               <p className={styles.serviceDescription}>
                 Vision stratégique et financière 3/5 ans. <br></br>
